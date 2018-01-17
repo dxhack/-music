a636e74aa0b8c22f980ac0668d77e8f288e2b978
dxhack <601239216@qq.com>
2018-01-17 10:19:37 +0800

    del

--- playlist.php
+++
@@ -1,9 +0,0 @@
-<?php  
-    $searchUrl = 'http://music.163.com/api/playlist/detail?id=';  
-    if(!empty($_GET['id']))  
-    {  
-        $searchUrl .= $_GET['id']; 
-    }
-    header('Content-Type:application/json;charset=UTF-8');
-    echo file_get_contents($searchUrl);  
-?> 
